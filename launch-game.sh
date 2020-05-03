609f6933d0207ab4b02995021b9a7a7055be47df
Shaham Noorani <shahamnoorani@gmail.com>
2020-05-02 20:20:02 -0500

    messing around with bash script

--- launch-game.sh
+++ launch-game.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-python -m http.server
+dir=$(find . -name Typersion/ -type d 2>/dev/null | head -1)
+
+cd $dir
 open -a "/Applications/Google Chrome.app" "http://0.0.0.0:8000/"
+python3 -m http.server
